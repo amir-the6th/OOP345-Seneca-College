691a0f0552a2670ed0c3cde774f656b6ca87ea59
asabagh <asabagh@myseneca.ca>
2021-02-06 19:45:26 -0500

    Update Set.h

--- Workshop-3/Workshop-3/Set.h
+++ Workshop-3/Workshop-3/Set.h
@@ -20,25 +20,18 @@ namespace sdds {
 		T s_collection[N];	//statically allocated array of any datatype
 		size_t elemsInCollection{ 0 };	//current number of elements in the collection
 	public:
-		size_t size() const;	//returns the current number of elements in the collection
-		const T& get(size_t idx) const;	//returns a reference to the element at index idx of s_collection
-		void operator+=(const T& item);	//if the collection has capacity, adds a copy of item to the collection
+		size_t size() const {
+			return elemsInCollection;
+		}	//returns the current number of elements in the collection
+		const T& get(size_t idx) const {
+			return s_collection[idx];
+		}	//returns a reference to the element at index idx of s_collection
+		void operator+=(const T& item) {
+			if (this->size() < N) {
+				s_collection[size()] = item;
+				elemsInCollection++;
+			}
+		}	//if the collection has capacity, adds a copy of item to the collection
 	};
-
-	template<unsigned N, class T>
-	inline size_t Set<N, T>::size() const {
-		return elemsInCollection;
-	}
-	template<unsigned N, class T>
-	inline const T& Set<N, T>::get(size_t idx) const {
-		return s_collection[idx];
-	}
-	template<unsigned N, class T>
-	inline void Set<N, T>::operator+=(const T& item) {
-		if (this->size() < N) {
-			s_collection[size()] = item;
-			elemsInCollection++;
-		}
-	}
 }
 #endif
