f8e0b4c3107b3be9d48933ea922ea2d43d8bcdd0
asabagh <asabagh@myseneca.ca>
2021-02-06 19:21:03 -0500

    Create Pair Module

---
+++ Workshop-3/Workshop-3/Pair.h
@@ -0,0 +1,20 @@
+/********************************************
+///    Name:        Amirhossein Sabagh    ///
+///    Student#:    152956199             ///
+///    Email:       asabagh@myseneca.ca   ///
+///    Date:        2021-02-07            ///
+|*******************************************|
+|*********  Workshop 3 - part 1&2  *********|
+|*******************************************|
+|   I have done all the coding by myself    |
+|     and only copied the code that my      |
+|     professor provided to complete my     |
+|        workshops and assignments.         |
+********************************************/
+
+#ifndef SDDS_PAIR_H
+#define SDDS_PAIR_H
+namespace sdds {
+
+}
+#endif
