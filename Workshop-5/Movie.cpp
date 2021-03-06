3914894265db226791da63b2461bb2225eeda2d7
asabagh <asabagh@myseneca.ca>
2021-03-06 09:52:34 -0500

    Create Movie Module

---
+++ Workshop-5/Movie.cpp
@@ -0,0 +1,15 @@
+/********************************************
+///    Name:        Amirhossein Sabagh    ///
+///    Student#:    152956199             ///
+///    Email:       asabagh@myseneca.ca   ///
+///    Date:        2021-03-06            ///
+|*******************************************|
+|*********  Workshop 5 - part 1&2  *********|
+|*******************************************|
+|   I have done all the coding by myself    |
+|     and only copied the code that my      |
+|     professor provided to complete my     |
+|        workshops and assignments.         |
+********************************************/
+
+#include <iostream>
---
+++ Workshop-5/Movie.h
@@ -0,0 +1,26 @@
+/********************************************
+///    Name:        Amirhossein Sabagh    ///
+///    Student#:    152956199             ///
+///    Email:       asabagh@myseneca.ca   ///
+///    Date:        2021-03-06            ///
+|*******************************************|
+|*********  Workshop 5 - part 1&2  *********|
+|*******************************************|
+|   I have done all the coding by myself    |
+|     and only copied the code that my      |
+|     professor provided to complete my     |
+|        workshops and assignments.         |
+********************************************/
+
+#ifndef SDDS_MOVIE_H
+#define SDDS_MOVIE_H
+#include <iostream>
+namespace sdds {
+	typedef std::string string;
+	class Movie {
+		string m_title; //title
+		size_t m_year; //
+		string m_description;
+	};
+}
+#endif
