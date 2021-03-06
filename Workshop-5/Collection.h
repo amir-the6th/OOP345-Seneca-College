1389078880a6776ad2fdd42a682f4980c8730eec
asabagh <asabagh@myseneca.ca>
2021-03-06 10:15:23 -0500

    Create Collection.h

---
+++ Workshop-5/Collection.h
@@ -0,0 +1,22 @@
+/********************************************
+///    Name:        Amirhossein Sabagh    ///
+///    Student#:    152956199             ///
+///    Email:       asabagh@myseneca.ca   ///
+///    Date:        2021-03-06            ///
+|*******************************************|
+|*********  Workshop 5 - part 1&2  *********|
+|*******************************************|
+|   I have done all the coding by myself    |
+|     and only copied the code that my      |
+|     professor provided to complete my     |
+|        workshops and assignments.         |
+********************************************/
+
+#ifndef SDDS_COLLECTION_H
+#define SDDS_COLLECTION_H
+#include <iostream>
+#include <string>
+namespace sdds {
+	typedef std::string string;
+}
+#endif
