9decf17dfbfa60ba5443dc3fcc80d01db5962913
asabagh <asabagh@myseneca.ca>
2021-03-05 21:25:02 -0500

    Create Book Module

---
+++ Workshop-5/Book.h
@@ -0,0 +1,40 @@
+/********************************************
+///    Name:        Amirhossein Sabagh    ///
+///    Student#:    152956199             ///
+///    Email:       asabagh@myseneca.ca   ///
+///    Date:        2021-03-05            ///
+|*******************************************|
+|*********  Workshop 5 - part 1&2  *********|
+|*******************************************|
+|   I have done all the coding by myself    |
+|     and only copied the code that my      |
+|     professor provided to complete my     |
+|        workshops and assignments.         |
+********************************************/
+
+#ifndef SDDS_BOOK_H
+#define SDDS_BOOK_H
+#include <iostream>
+#include <string>
+namespace sdds {
+	typedef std::string string;
+	class Book {
+		string b_author{};
+		string b_title{};
+		string b_country{};
+		size_t b_year{ 0 };
+		double b_price{ 0 };
+		string b_description{};
+	public:
+		Book() {};
+		const std::string& author() const;
+		const std::string& title() const;
+		const std::string& country() const;
+		const size_t& year() const;
+		double& price();
+		const std::string& description() const;
+		Book(const std::string& strBook);
+		friend std::ostream& operator<<(std::ostream& os, const Book& bck);
+	};
+}
+#endif
