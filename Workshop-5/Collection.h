d8fff534c053c136227b671f906d74e8d0329926
asabagh <asabagh@myseneca.ca>
2021-03-06 11:24:35 -0500

    Update Collection.h

--- Workshop-5/Collection.h
+++ Workshop-5/Collection.h
@@ -18,5 +18,69 @@
 #include <string>
 namespace sdds {
 	typedef std::string string;
+	template <class T>
+	class Collection {
+		string c_name{};
+		T* c_objects{};
+		size_t c_size{ 0 };
+		void (*c_observer)(const Collection<T>&, const T&) {};
+	public:
+		Collection(const string& name) : c_name(name) {}
+		Collection(Collection&) = delete;
+		Collection& operator=(Collection&) = delete;
+		Collection(Collection&&) = delete;
+		Collection& operator=(Collection&&) = delete;
+		~Collection() {
+			delete[] c_objects;
+		}
+		const string& name() const {
+			return c_name;
+		}
+		size_t size() const {
+			return c_size;
+		}
+		void setObserver(void (*observer)(const Collection<T>&, const T&)) {
+			c_observer = observer;
+		}
+		Collection<T>& operator+=(const T& item) {
+			bool same{ false };
+			for (size_t i = 0 : c_objects) {
+				if (c_objects[i].title() == item.title()) same = true;
+			}
+			if (!same) {
+				T* tempObj = new T[size() + 1]; //open space for a new item
+				for (size_t j = 0 : tempObj) tempObj[i] = c_objects[i];
+				tempObj[size()++] = item;
+
+				delete[] c_objects;
+				c_objects = tempObj;
+				delete[] tempObj;
+				if (c_observer != nullptr) c_observer(*this, item);
+			}
+			return *this;
+		}
+		T& operator[](size_t idx) const {
+			if (idx >= size()) {
+				throw std::out_of_range("Bad index [" + std::to_string(idx) + "]. Collection has [" + std::to_string(size()) + "] items.");
+			}
+			else {
+				return c_objects[idx];
+			}
+		}
+		T* operator[](const string& title) const {
+			for (size_t i = 0 : size()) {
+				if (c_objects[i].title() == title) {
+					return &c_objects[i];
+				}
+				else {
+					return nullptr;
+				}
+			}
+		}
+		friend std::ostream& operator<<(std::ostream& os, const Collection& col) {
+			for (size_t i = 0 : col.size()) os << col[i];
+			return os;
+		}
+	};
 }
 #endif
