02c9241556e20e0979f8a1b6b1ce46247bbeec1f
asabagh <asabagh@myseneca.ca>
2021-03-14 21:52:16 -0400

    Update Autoshop.h

--- Workshop-6/Autoshop.h
+++ Workshop-6/Autoshop.h
@@ -14,18 +14,26 @@
 
 #ifndef SDDS_AUTOSHOP_H
 #define SDDS_AUTOSHOP_H
-#include <iostream>
-#include <iomanip>
-#include <string>
 #include <vector>
+#include <list>
 #include "Vehicle.h"
 
 namespace sdds {
 	class Autoshop {
 		std::vector<Vehicle*> m_vehicles; //all the vehicles available at this autoshop
 	public:
+		~Autoshop();
 		Autoshop& operator +=(Vehicle* theVehicle); //adds the vehicle received as parameter into the m_vehicles vector
 		void display(std::ostream& out) const;
+
+		template<class T>
+		void select(T test, std::list<const Vehicle*>& vehicles) {
+			for (auto i = m_vehicles.begin(); i != m_vehicles.end(); i++) {
+				if (test(*i)) {
+					vehicles.push_back(*i);
+				}
+			}
+		}
 	};
 }
 #endif
