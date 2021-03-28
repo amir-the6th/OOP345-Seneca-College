96f9363eb2d0ab5936ab0a0760043aea0a7312ff
asabagh <asabagh@myseneca.ca>
2021-03-27 22:46:04 -0400

    Update Station.cpp

--- Final Project/MS1/Station.cpp
+++ Final Project/MS1/Station.cpp
@@ -13,7 +13,32 @@
 #include "Station.h"
 
 namespace sdds {
+    size_t Station::m_widthField = 0;
+    size_t Station::id_generator = 0;
+
+    Station::Station(const string& str) {
+    }
     const string& Station::getItemName() const {
-        // TODO: insert return statement here
+        return s_name;
+    }
+    size_t Station::getNextSerialNumber() {
+        return s_nextSerialNo++;
+    }
+    size_t Station::getQuantity() const {
+        return s_inStock;
+    }
+    void Station::updateQuantity() {
+        s_inStock = s_inStock < 0 ? 0 : s_inStock - 1;
+    }
+    void Station::display(std::ostream& os, bool full) const {
+        os << '[' << std::setw(3) << std::setfill('0') << std::right << s_id
+            << "] Item: " << std::setw(m_widthField) << std::setfill(' ') << std::left << s_name
+            << " [" << std::setw(6) << std::setfill('0') << std::right << s_nextSerialNo << "]" 
+            << std::setfill(' ') << std::left;
+        if (full) {
+            os << " Quantity: " << std::setw(m_widthField) << s_inStock
+                << " Description: " << s_description;
+        }
+        os << std::endl;
     }
 }
