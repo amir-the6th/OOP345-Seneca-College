157b16b8823ed22236508e639abd43d572991991
asabagh <asabagh@myseneca.ca>
2021-03-27 22:33:51 -0400

    Update class variables

--- Final Project/MS1/Utilities.h
+++ Final Project/MS1/Utilities.h
@@ -21,8 +21,8 @@ namespace sdds{
 	typedef std::string string;
 	class Utilities {
 		size_t m_widthField{ 1 }; //specifies the length of the token extracted
-	public:
 		static char m_delimiter; //separates the tokens in any given std::string object
+	public:
 		void setFieldWidth(size_t newWidth); //sets the field width of the current object to the value of parameter
 		size_t getFieldWidth() const; //returns the field width of the current object
 		std::string extractToken(const std::string& str, size_t& next_pos, bool& more); //extracts a token from str referred to by the first param
