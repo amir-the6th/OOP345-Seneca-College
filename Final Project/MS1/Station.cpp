d6e40b69faa28c4f68c7c7654b52fcc944cfeb6f
asabagh <asabagh@myseneca.ca>
2021-03-27 22:34:18 -0400

    Create Station Module

---
+++ Final Project/MS1/Station.cpp
@@ -0,0 +1,19 @@
+/********************************************
+///    Name:        Amirhossein Sabagh    ///
+///    Student#:    152956199             ///
+///    Email:       asabagh@myseneca.ca   ///
+///    Date:        2021-03-27            ///
+|*******************************************|
+|**********  Final Project - MS1  **********|
+|*******************************************|
+|    I confirm that I have done all the     |
+|    coding by myself and that I am the     |
+//       only author of this file.	        |
+********************************************/
+#include "Station.h"
+
+namespace sdds {
+    const string& Station::getItemName() const {
+        // TODO: insert return statement here
+    }
+}
---
+++ Final Project/MS1/Station.h
@@ -0,0 +1,38 @@
+/********************************************
+///    Name:        Amirhossein Sabagh    ///
+///    Student#:    152956199             ///
+///    Email:       asabagh@myseneca.ca   ///
+///    Date:        2021-03-27            ///
+|*******************************************|
+|**********  Final Project - MS1  **********|
+|*******************************************|
+|    I confirm that I have done all the     |
+|    coding by myself and that I am the     |
+//       only author of this file.	        |
+********************************************/
+
+#ifndef SDDS_STATION_H
+#define SDDS_STATION_H
+#include <iostream>
+#include <string>
+
+namespace sdds {
+	typedef std::string string;
+	class Station {
+		size_t s_id{}; //the id of the station
+		string s_name{}; //the name of the item handled by the station
+		string s_description{}; //the description of the station
+		size_t s_nextNo{}; //the next serial number to be assigned to an item at this station
+		size_t s_inStock{}; //the number of items currently in stock
+		static size_t m_widthField; // the maximum number of characters required to print to the screen
+		static size_t id_generator; //a variable used to generate IDs for new instances of type Station
+	public:
+		Station(const string& str);
+		const string& getItemName() const;
+		size_t getNextSerialNumber();
+		size_t getQuantity() const;
+		void updateQuantity();
+		void display(std::ostream& os, bool full) const;
+	};
+}
+#endif
