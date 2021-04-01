0e770a328c9fecc4a13c90333175b03011317e3a
asabagh <asabagh@myseneca.ca>
2021-03-31 20:20:02 -0400

    Implement mergeRaw function

--- Workshop-8/Utilities.cpp
+++ Workshop-8/Utilities.cpp
@@ -14,7 +14,16 @@ namespace sdds {
 		List<Product> priceList;
 		// TODO: Add your code here to build a list of products
 		//         using raw pointers
-
+		for (size_t i = 0; i < desc.size(); i++) {
+			for (size_t j = 0; j < price.size(); j++) {
+				if (desc[i].code == price[i].code) {
+					Product* p = new Product(desc[i].desc, price[j].price);
+					p->validate(); //call validate() function in Element.h to check the validity of the price
+					priceList += p; //call += operator overload in List.h
+					delete p;
+				}
+			}
+		}
 
 
 		return priceList;
