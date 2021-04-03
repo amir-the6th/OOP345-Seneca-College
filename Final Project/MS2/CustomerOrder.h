e033c4f761b4eaef0f1401307fb55a85d9d7372f
asabagh <asabagh@myseneca.ca>
2021-04-03 10:03:11 -0400

    MS2 Project Init

---
+++ Final Project/MS2/CustomerOrder.h
@@ -0,0 +1,22 @@
+/********************************************
+///    Name:        Amirhossein Sabagh    ///
+///    Student#:    152956199             ///
+///    Email:       asabagh@myseneca.ca   ///
+///    Date:        2021-04-03            ///
+|*******************************************|
+|**********  Final Project - MS2  **********|
+|*******************************************|
+|    I confirm that I have done all the     |
+|    coding by myself and that I am the     |
+//       only author of this file.	        |
+********************************************/
+#ifndef SDDS_CUSTOMERORDER_H
+#define SDDS_CUSTOMERORDER_H
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+namespace sdds {
+
+}
+#endif
