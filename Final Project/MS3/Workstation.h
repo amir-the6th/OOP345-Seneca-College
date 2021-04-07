0189478e92ee612c55353cbcf918491146c1c6d9
asabagh <asabagh@myseneca.ca>
2021-04-07 18:33:41 -0400

    Init Workstation Module

---
+++ Final Project/MS3/Workstation.h
@@ -0,0 +1,17 @@
+/********************************************
+///    Name:        Amirhossein Sabagh    ///
+///    Student#:    152956199             ///
+///    Email:       asabagh@myseneca.ca   ///
+///    Date:        2021-04-10            ///
+|*******************************************|
+|**********  Final Project - MS3  **********|
+|*******************************************|
+|    I confirm that I have done all the     |
+|    coding by myself and that I am the     |
+//       only author of this file.	        |
+********************************************/
+#ifndef SDDS_WORKSTATION_H
+#define SDDS_WORKSTATION_H
+namespace ssds {
+}
+#endif
