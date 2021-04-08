60373f3305485e02edceb9ecd4983435b2560e33
asabagh <asabagh@myseneca.ca>
2021-04-07 20:59:30 -0400

    Update Workstation Module

---
+++ Final Project/MS3/Workstation.cpp
@@ -0,0 +1,51 @@
+/********************************************
+///    Name:        Amirhossein Sabagh    ///
+///    Student#:    152956199             ///
+///    Email:       asabagh@myseneca.ca   ///
+///    Date:        2021-04-10            ///
+|*******************************************|
+|**********  Final Project - MS3  **********|
+|*******************************************|
+|    I confirm that I have done all the     |
+|    coding by myself and that I am the     |
+//       only author of this file.	        |
+********************************************/
+#include "Workstation.h"
+
+namespace sdds {
+	void Workstation::fill(std::ostream& os) {
+		// this modifier fills the order at the front of the queue, 
+		// if there are CustomerOrders in the queue; otherwise, does nothing.
+		if (!m_orders.empty()) {
+			m_orders.front().fillItem(*this, os);
+		}
+	}
+	bool Workstation::attemptToMoveOrder() {
+		//attempts to move the order at the front of the queue to the next station in the assembly line:
+		bool moved{ false };
+
+		
+		return moved;
+	}
+	void Workstation::setNextStation(Workstation* station) {
+		//this modifier stores the address of the referenced Workstation object in the pointer to the m_pNextStation
+		m_pNextStation = (Workstation *)&station;
+	}
+	Workstation* Workstation::getNextStation() const {
+		// returns the address of next Workstation
+		return m_pNextStation;
+	}
+	void Workstation::display(std::ostream& os) const {
+		if (m_pNextStation) {
+			os << getItemName() << " --> " << m_pNextStation->getItemName() << std::endl;
+		}
+		else {
+			os << getItemName() << " --> " << "END OF LINE" << std::endl;
+		}
+	}
+	Workstation& Workstation::operator+=(CustomerOrder&& newOrder) {
+		// moves the CustomerOrder referenced in parameter newOrder to the back of the queue
+		m_orders.push_back(std::move(newOrder));
+		return *this;
+	}
+}
--- Final Project/MS3/Workstation.h
+++ Final Project/MS3/Workstation.h
@@ -12,6 +12,32 @@
 ********************************************/
 #ifndef SDDS_WORKSTATION_H
 #define SDDS_WORKSTATION_H
-namespace ssds {
+#include <deque>
+#include "CustomerOrder.h"
+#include "Station.h"
+
+namespace sdds {
+	std::deque<CustomerOrder> pending; //holds the orders to be placed onto the assembly line at the first station
+	std::deque<CustomerOrder> completed; //holds the orders that have been removed from the last station and have been completely filled
+	std::deque<CustomerOrder> incomplete; //holds the orders that have been removed from the last station and could not be filled completely
+	class Workstation : public Station {
+		std::deque<CustomerOrder> m_orders; //orders that have been placed on this station to receive service (or already received service)
+		Workstation* m_pNextStation{ nullptr }; //a pointer to the next Workstation on the assembly line
+	public:
+		Workstation(std::string str) : Station(str) {};
+		void fill(std::ostream& os);
+		bool attemptToMoveOrder();
+		void setNextStation(Workstation* station = nullptr);
+		Workstation* getNextStation() const;
+		void display(std::ostream& os) const;
+		Workstation& operator+=(CustomerOrder&& newOrder);
+
+		/*Since a Workstation object represents a single location on the assembly line
+		for filling customer orders with items, the object cannot be copied or moved.*/
+		Workstation(const Workstation&) = delete;
+		Workstation& operator=(Workstation&) = delete;
+		Workstation(Workstation&&) noexcept = delete;
+		Workstation& operator=(Workstation&&) noexcept = delete;
+	};
 }
 #endif
