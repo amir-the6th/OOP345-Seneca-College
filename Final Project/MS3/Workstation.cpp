417f337e0e51d75e7147975876a308ba0fb17a79
asabagh <asabagh@myseneca.ca>
2021-04-13 13:26:57 -0400

    Complete attemptToMoveOrder() function

--- Final Project/MS3/Workstation.cpp
+++ Final Project/MS3/Workstation.cpp
@@ -2,7 +2,7 @@
 ///    Name:        Amirhossein Sabagh    ///
 ///    Student#:    152956199             ///
 ///    Email:       asabagh@myseneca.ca   ///
-///    Date:        2021-04-10            ///
+///    Date:        2021-04-13            ///
 |*******************************************|
 |**********  Final Project - MS3  **********|
 |*******************************************|
@@ -13,6 +13,10 @@
 #include "Workstation.h"
 
 namespace sdds {
+	std::deque<CustomerOrder> pending;
+	std::deque<CustomerOrder> completed;
+	std::deque<CustomerOrder> incomplete;
+
 	void Workstation::fill(std::ostream& os) {
 		// this modifier fills the order at the front of the queue, 
 		// if there are CustomerOrders in the queue; otherwise, does nothing.
@@ -23,14 +27,37 @@ namespace sdds {
 	bool Workstation::attemptToMoveOrder() {
 		//attempts to move the order at the front of the queue to the next station in the assembly line:
 		bool moved{ false };
-
-		
+		if (!m_orders.empty()) {
+			if (m_orders.front().isItemFilled(getItemName())) {
+				if (m_pNextStation == nullptr) {
+					completed.push_back(std::move(m_orders.front()));
+					m_orders.pop_front();
+					moved = true;
+				}
+				else {
+					*m_pNextStation += std::move(m_orders.front());
+					m_orders.pop_front();
+					moved = true;
+				}
+			}
+			else if (this->getQuantity() <= 0) {
+				if (m_pNextStation == nullptr) {
+					incomplete.push_back(std::move(m_orders.front()));
+					m_orders.pop_front();
+					moved = true;
+				}
+				else {
+					*m_pNextStation += std::move(m_orders.front());
+					m_orders.pop_front();
+					moved = true;
+				}
+			}
+		}
 		return moved;
 	}
 	void Workstation::setNextStation(Workstation* station) {
 		//this modifier stores the address of the referenced Workstation object in the pointer to the m_pNextStation
 		m_pNextStation = station;
-		//m_pNextStation = (Workstation *)&station;
 	}
 	Workstation* Workstation::getNextStation() const {
 		// returns the address of next Workstation
@@ -41,7 +68,7 @@ namespace sdds {
 			os << getItemName() << " --> " << m_pNextStation->getItemName() << std::endl;
 		}
 		else {
-			os << getItemName() << " --> " << "END OF LINE" << std::endl;
+			os << getItemName() << " --> " << "End Of Line" << std::endl;
 		}
 	}
 	Workstation& Workstation::operator+=(CustomerOrder&& newOrder) {
